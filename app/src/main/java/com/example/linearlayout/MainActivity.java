2fe43a97ba2820de8bf8b8b81b3cb99dd4e060f8
ROHITGORULE <rohitgorule05@gmail.com>
2024-01-12 16:30:42 +0530

    Linearlayout

--- app/src/main/java/com/example/linearlayout/MainActivity.java
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.linearlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText ede = (EditText)findViewById(R.id.ede);
+        final EditText ede1 =(EditText)findViewById(R.id.ede1);
+        Button btnadd = (Button)findViewById(R.id.btnn);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"name="+"marks=",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
